c4c67ed57c12577594a9177fe53d448d51f66ee4
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2023-10-29 20:26:30 +0200

    task 02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const myList = document.querySelector('ul.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItems =>
+      `<li class="gallery__item"><a class="gallery__link" href=${galleryItems.original} ><img class="gallery__image" src = ${galleryItems.preview} alt = ${galleryItems.description} /> </a></li>`
+  )
+  .join('');
+myList.insertAdjacentHTML('beforeend', markup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
